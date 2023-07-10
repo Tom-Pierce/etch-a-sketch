073a43d680f6d6d4dacd7a6665e3ed4b8fb9a482
Tom Pierce <tompierce4@gmail.com>
2023-07-10 12:20:20 +0100

    Change the canvas size via a slider
    A responsive slider now controls the size of the canvas and the reset button clears the canvas.
    Added comments to make the code more readable

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 function createCanvas(size) {
 
+    // set the main container to "canvas"
     const canvas = document.querySelector("#canvas");
 
-    for (i = 0; i < size; i++) {
-        for (j = 0; j < size; j++) {
+    //loops through to make each "pixel" and appends it to the canvas
+        for (j = 0; j < size*size; j++) {
             const div = document.createElement("div");
             div.classList.add("pixel");
             canvas.appendChild(div);
 
         }
-    }
     listenForMouse();
 }
 
+// add a mouseover event listener to each pixel
 function listenForMouse() {
 
     let pixels = document.querySelectorAll(".pixel");
@@ -25,7 +26,7 @@ function listenForMouse() {
 }
 
 function changeCanvasSize() {
-    const canvasSize = prompt("What size canvas would you like?");
+    const canvasSize = document.getElementById("canvasSize").value;
     clearCanvas();
     createCanvas(canvasSize);
     changePixelSize(canvasSize);
@@ -33,20 +34,34 @@ function changeCanvasSize() {
 
 function changePixelSize(canvasSize) {
 
+    //loops through each pixel and makes it fit into the canvas
     let pixels = document.querySelectorAll(".pixel");
-    pixelWidth = 960/canvasSize;
-    pixelHeight = 960/canvasSize;
+    pixelWidth = 960 / canvasSize;
+    pixelHeight = 960 / canvasSize;
     for (i = 0; i < pixels.length; i++) {
         pixels[i].style.width = pixelWidth + "px";
-        pixels[i].style.height = pixelHeight +"px";
-        }
+        pixels[i].style.height = pixelHeight + "px";
     }
+}
 
 function clearCanvas() {
+
+    //removes every single appended node to clear the canvas
     const canvas = document.querySelector("#canvas");
     while (canvas.lastChild) {
         canvas.removeChild(canvas.lastChild);
     }
 }
 
+function resetCanvas() {
+
+    //sets all pixels to white to reset the canvas
+    let pixels = document.querySelectorAll(".pixel");
+
+    for (i = 0; i < pixels.length; i++) {
+        pixels[i].style.backgroundColor = "white";
+
+    }
+}
+//initializes the canvas
 createCanvas(16);
